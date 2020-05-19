728534493217ec1a21530af3ae35f937b0cd805a
saiyedaws <saiyedaws@gmail.com>
2020-05-19 03:53:28 -0500

    fix date html bug

--- amazon/amazon_place_order.js
+++ amazon/amazon_place_order.js
@@ -39,8 +39,30 @@ function getDeliveryDate()
 
    deliveryDate = date.innerText;
 
-    return deliveryDate;
+
+    return decodeHTMLEntities(deliveryDate);
 };
+
+function decodeHTMLEntities(text) 
+{
+    var entities = [
+        ['amp', '&'],
+        ['apos', '\''],
+        ['#x27', '\''],
+        ['#x2F', '/'],
+        ['#39', '\''],
+        ['#47', '/'],
+        ['lt', '<'],
+        ['gt', '>'],
+        ['nbsp', ' '],
+        ['quot', '"']
+    ];
+
+    for (var i = 0, max = entities.length; i < max; ++i) 
+        text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+
+    return text;
+}
 
 
 function getAmazonDeliveryData(){
